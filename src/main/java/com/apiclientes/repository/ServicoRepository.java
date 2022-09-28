39e77f224686a47e269c094d54c17e2e42213c28
Andressa Santos <andressasantosp0@gmail.com>
2022-09-27 21:09:19 -0300

    feat: criando repository de serviço

---
+++ src/main/java/com/apiclientes/repository/ServicoRepository.java
@@ -0,0 +1,9 @@
+package com.apiclientes.repository;
+
+import com.apiclientes.model.Servico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServicoRepository extends JpaRepository<Servico, Long> {
+}
