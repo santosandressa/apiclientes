0cf82b435debeb7a958dd56c61fbfb1baed42f0e
Andressa Santos <andressasantosp0@gmail.com>
2022-09-27 21:00:31 -0300

    feat: criando repository de cliente

---
+++ src/main/java/com/apiclientes/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.apiclientes.repository;
+
+import com.apiclientes.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+}
