9dc5600fc9741bbae48ecb32c96d98be3d82fa99
Andressa Santos <andressasantosp0@gmail.com>
2022-09-30 16:19:23 -0300

    feat: buscando endereço por cep - feign

--- src/main/java/com/apiclientes/ApiClientesApplication.java
+++ src/main/java/com/apiclientes/ApiClientesApplication.java
@@ -2,8 +2,10 @@ package com.apiclientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ApiClientesApplication {
 
     public static void main(String[] args) {
